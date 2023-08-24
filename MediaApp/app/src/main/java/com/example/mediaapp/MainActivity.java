6861c8467ae53004241b1690d3e9bf5bcd347d7c
Nimish Gontiya <36588657+nim-ish@users.noreply.github.com>
2023-08-24 01:03:49 -0400

    Add files via upload

---
+++ MediaApp/app/src/main/java/com/example/mediaapp/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play=findViewById(R.id.play);
+        pause=findViewById(R.id.pause);
+        seekBar=findViewById(R.id.seekBar);
+        //using local source
+        //mediaPlayer=MediaPlayer.create(this, R.raw.maine_pi_rakhi_hai);
+
+        //using internet
+        mediaPlayer= new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("http://penguinradio.dominican.edu/Sound%20FX%20Collection/Police%20Car%20Siren%20(Sound%20Effect).mp3");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+//        mediaPlayer.start();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Ready to Play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                        if(b) {
+                            mediaPlayer.seekTo(i);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+        mediaPlayer.prepareAsync();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
